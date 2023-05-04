254839ae479580405a73a2e764a7a8f31a71e4a7
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-05-04 12:26:47 -0300

    DB connect

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const server = express();
-const routes = require('./routes/index')
-const morgan = require('morgan')
+const routes = require('./routes/index');
+const { conn } = require('./DB_connection');
+const morgan = require('morgan');
 
 
 server.use((req, res, next) => {
@@ -22,7 +23,9 @@ server.use((req, res, next) => {
  server.use(express.json());
  server.use("/rickandmorty", routes)
         
-                
-        
-const PORT = 3001;
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+ const PORT = 3001;
+ 
+conn.sync({alter: true}).then(() => {
+    server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+})
+.catch((error) => {console.log(error)});  
